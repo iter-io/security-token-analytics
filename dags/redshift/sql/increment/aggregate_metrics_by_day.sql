9bca59f512596ae12079833f9ebe2ca197723f31
Webster <webster@iteriodata.com>
2019-02-07 18:23:51 -0800

    Format SQL

--- dags/redshift/sql/increment/aggregate_metrics_by_day.sql
+++ dags/redshift/sql/increment/aggregate_metrics_by_day.sql
@@ -12,22 +12,22 @@ CREATE TABLE ethereum.aggregate_metrics_by_day_incr_tmp
 INSERT INTO ethereum.aggregate_metrics_by_day_incr_tmp
 SELECT
   DATE_TRUNC('day', TIMESTAMP 'epoch' + blocks.timestamp * INTERVAL '1 second')
-                                            AS day,
+                                AS day,
 
-  COUNT(DISTINCT blocks.hash)               AS blocks_cnt,
-  COUNT(DISTINCT blocks.miner)              AS unique_miners,
-  AVG(blocks.difficulty)                    AS median_difficulty,
-  MAX(blocks.total_difficulty)              AS cumulative_difficulty,
-  SUM(blocks.size)                          AS total_blocksize_bytes,
-  SUM(blocks.gas_used)                      AS gas_used,
-  SUM(transaction_count)                    AS transactions_cnt_from_blocks,
-  SUM(transactions_cnt)                     AS transactions_cnt,
-  SUM(new_addresses)                        AS new_addresses,
-  SUM(unique_senders)                       AS unique_senders,
-  SUM(unique_receivers)                     AS unique_receivers,
-  SUM(value_transferred_wei)                AS value_transferred_wei,
-  SUM(value_transferred_eth)                AS value_transferred_eth,
-  SUM(total_gas_provided)                   AS total_gas_provided
+  COUNT(DISTINCT blocks.hash)   AS blocks_cnt,
+  COUNT(DISTINCT blocks.miner)  AS unique_miners,
+  AVG(blocks.difficulty)        AS median_difficulty,
+  MAX(blocks.total_difficulty)  AS cumulative_difficulty,
+  SUM(blocks.size)              AS total_blocksize_bytes,
+  SUM(blocks.gas_used)          AS gas_used,
+  SUM(transaction_count)        AS transactions_cnt_from_blocks,
+  SUM(transactions_cnt)         AS transactions_cnt,
+  SUM(new_addresses)            AS new_addresses,
+  SUM(unique_senders)           AS unique_senders,
+  SUM(unique_receivers)         AS unique_receivers,
+  SUM(value_transferred_wei)    AS value_transferred_wei,
+  SUM(value_transferred_eth)    AS value_transferred_eth,
+  SUM(total_gas_provided)       AS total_gas_provided
 FROM
   ethereum.blocks AS blocks
 INNER JOIN
